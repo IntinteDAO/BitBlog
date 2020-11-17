244657a557e56d5867b684eea81b1b25ed32494b
fervi <lordfervi@protonmail.com>
2020-11-17 23:48:45 +0100

    Add session protection

--- config.php
+++ config.php
@@ -8,5 +8,34 @@ $recovery_phases_quantity = 12;
 
 $tip_enable = 1;
 
-session_start();
+// Bootstrap
+    header('X-Frame-Options: SAMEORIGIN');
+
+
+    if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+        $secure = true;
+    } else {
+        $secure = false;
+    }
+
+    $httponly = true;
+    $samesite = 'strict';
+    $maxlifetime = 60 * 60 * 24 * 30;
+
+if(php_sapi_name()!="cli") {
+    if(PHP_VERSION_ID < 70300) {
+        session_set_cookie_params($maxlifetime, '/; samesite='.$samesite, $_SERVER['HTTP_HOST'], $secure, $httponly);
+    } else {
+        session_set_cookie_params([
+            'lifetime' => $maxlifetime,
+            'path' => '/',
+            'domain' => $_SERVER['HTTP_HOST'],
+            'secure' => $secure,
+            'httponly' => $httponly,
+            'samesite' => $samesite
+        ]);
+    }
+}
+
+    session_start();
 ?>
