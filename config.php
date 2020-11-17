8f94152037e3e4a6103a723f4dee8c280a6365d6
fervi <lordfervi@protonmail.com>
2020-11-17 23:38:37 +0100

    Updated variables in config.php

--- config.php
+++ config.php
@@ -2,7 +2,9 @@
 
 $website_title = "Intinte BitBlog";
 $salt = '';
+$iv = ''; // AES 256 IV required and stored by base64 [ base64_encode(openssl_random_pseudo_bytes(16)); ]
 $node = 'bitblog.intinte.org'; // Node ID, recommended to set domain
+$recovery_phases_quantity = 12;
 
 $tip_enable = 1;
 
