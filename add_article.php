9d3a9c13359ba31a21f466a8ca17a09257ff6f2c
fervi <lordfervi@protonmail.com>
2020-11-18 13:47:20 +0100

    Adding articles: Authorization requirement, improving file formatting

--- add_article.php
+++ add_article.php
@@ -7,71 +7,65 @@ include('functions/editor.php');
 include('functions/verify_html.php');
 include('functions/tag_verify.php');
 
+if(empty($_SESSION['login'])) { die('You are not logged in'); }
+
 if( !empty($_POST['title']) && !empty($_POST['text']) && !empty($_POST['tags']) ) {
-$error = 0;
-if(strlen($_POST['text']) > 1000000) {
-	echo ("Too much data. Delete a few pictures or move them to another hosting");
-	$error = 1;
-}
 
-/*if(tag_verify($_POST['tags']) == 0) {
-	echo ('Tags not in accordance with the rules. It is supposed to have up to 24 lower case letters (e.g. "bitcoin"). It can have a dash in 3 characters (e.g. "pl-bitcoin").');
-	$error = 1;
-}*/
+	$error = 0;
 
-if($error == 0) {
-$save_to_file['creator'] = "fervi";
-$save_to_file['node'] = $node;
-
-$verify_tags = explode(',', $_POST['tags']);
-$tags = [];
-for($i=0; $i<=count($verify_tags)-1; $i++) {
-
-	if(tag_verify($verify_tags[$i])==1) {
-		array_push($tags, $verify_tags[$i]);
-
+	if(strlen($_POST['text']) > 1000000) {
+		echo ("Too much data. Delete a few pictures or move them to another hosting");
+		$error = 1;
 	}
 
-}
+	if($error == 0) {
+		$save_to_file['creator'] = $_SESSION['login'];
+		$save_to_file['node'] = $node;
 
-if(empty($tags)) { die(); }
+		$verify_tags = explode(',', $_POST['tags']);
+		$tags = [];
 
-$save_to_file['created'] = time();
-$save_to_file['last_update'] = time();
-$save_to_file['title'] = $_POST['title'];
-$save_to_file['body'] = str_replace('</body></html>', '', str_replace('<html><body>', '', str_replace('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">', '', verify_html($_POST['text']))));
-$save_to_file['tags'] = $tags;
-$json = json_encode($save_to_file);
-$filename = hash("sha256", $json);
-$fp = fopen('articles/'.$filename.'.json', 'w');
-fwrite($fp, $json);
-fclose($fp);
+		for($i=0; $i<=count($verify_tags)-1; $i++) {
+			if(tag_verify($verify_tags[$i])==1) {
+				array_push($tags, $verify_tags[$i]);
+			}
+		}
 
-if(!file_exists('indexes/users/'.$save_to_file['creator'])) { mkdir('indexes/users/'.$save_to_file['creator']); }
-mkdir('indexes/comments/'.$filename);
+		if(empty($tags)) { die(); }
 
-for($i=0; $i<=count($tags)-1; $i++) {
+		$save_to_file['created'] = time();
+		$save_to_file['last_update'] = time();
+		$save_to_file['title'] = $_POST['title'];
+		$save_to_file['body'] = str_replace('</body></html>', '', str_replace('<html><body>', '', str_replace('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">', '', verify_html($_POST['text']))));
+		$save_to_file['tags'] = $tags;
+		$json = json_encode($save_to_file);
+		$filename = hash("sha256", $json);
+		$fp = fopen('articles/'.$filename.'.json', 'w');
+		fwrite($fp, $json);
+		fclose($fp);
 
-	if(!file_exists('indexes/tags/'.$tags[$i])) { mkdir('indexes/tags/'.$tags[$i]); }
-	symlink('../../../articles/'.$filename.'.json', 'indexes/tags/'.$tags[$i].'/'.$filename.'.json');
+		if(!file_exists('indexes/users/'.$save_to_file['creator'])) { mkdir('indexes/users/'.$save_to_file['creator']); }
+		mkdir('indexes/comments/'.$filename);
 
-}
+		for($i=0; $i<=count($tags)-1; $i++) {
+			if(!file_exists('indexes/tags/'.$tags[$i])) { mkdir('indexes/tags/'.$tags[$i]); }
+			symlink('../../../articles/'.$filename.'.json', 'indexes/tags/'.$tags[$i].'/'.$filename.'.json');
+		}
 
-symlink('../../../articles/'.$filename.'.json', 'indexes/users/'.$save_to_file['creator'].'/'.$filename.'.json');
-echo '<meta http-equiv="refresh" content="0; url=show_article.php?id='.$filename.'" />';
-}
+		symlink('../../../articles/'.$filename.'.json', 'indexes/users/'.$save_to_file['creator'].'/'.$filename.'.json');
+		echo '<meta http-equiv="refresh" content="0; url=show_article.php?id='.$filename.'" />';
+	}
 
 } else {
-
-echo '<div class="col-12"><form method="POST">
-<input id="Id" type="hidden" name="text" value="">
-<input class="form-control" type="text" name="title" placeholder="Title">';
-echo init_editor();
-echo '
-<input type="text" id="tags" name="tags" data-role="tagsinput" value="" placeholder="Tags">
-Tags - (like bitcoin, community, games) Use the prefix for language tags (pl-bitcoin, de-internet, fr-crypto etc.)<br>
-<button class="btn btn-primary" onclick="document.getElementById(\'Id\').value=$(\'#summernote\').summernote(\'code\');">Add comment</button>
-</form></div>';
+	echo '<div class="col-12"><form method="POST">
+	<input id="Id" type="hidden" name="text" value="">
+	<input class="form-control" type="text" name="title" placeholder="Title">';
+	echo init_editor();
+	echo '
+	<input type="text" id="tags" name="tags" data-role="tagsinput" value="" placeholder="Tags">
+	Tags - (like bitcoin, community, games) Use the prefix for language tags (pl-bitcoin, de-internet, fr-crypto etc.)<br>
+	<button class="btn btn-primary" onclick="document.getElementById(\'Id\').value=$(\'#summernote\').summernote(\'code\');">Add comment</button>
+	</form></div>';
 }
 
 include('footer.php');
