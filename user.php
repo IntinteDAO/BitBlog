4d96efa2c221a0496b4ded671fefa4c5c5989a36
fervi <lordfervi@protonmail.com>
2020-11-20 22:43:21 +0100

    Fix minor bug

--- user.php
+++ user.php
@@ -5,15 +5,13 @@ include('header.php');
 include('template/navbar.php');
 include('functions/show_articles.php');
 
-
-
 if(!empty($_GET['username'])) {
 
 	if(preg_match("@^([a-z]){3,32}$@", $_GET['username'])) {
 		if(!empty('indexes/users/'.$_GET['username'])) {
 			echo show_articles('indexes/users/'.$_GET['username']);
 		}
-
+	}
 }
 
 include('footer.php');
