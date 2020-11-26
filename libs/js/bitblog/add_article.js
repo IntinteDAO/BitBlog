20e4b942f27d2f01bf89f5fc96bf6f9fea6ab400
fervi <lordfervi@protonmail.com>
2020-11-26 23:38:19 +0100

    Fix disabling buttons bug

--- libs/js/bitblog/add_article.js
+++ libs/js/bitblog/add_article.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
-    $('.btn').attr('disabled',true);
+    $('#submit').attr('disabled',true);
     
 $(document).keyup(function() {
         if(($("#title").val().length != 0) && ($(".badge")[0])){
-            $('.btn').attr('disabled', false);
+            $('#submit').attr('disabled', false);
         }
         else
         {
-            $('.btn').attr('disabled', true);
+            $('#submit').attr('disabled', true);
         }
     })
 });
