138419a00413cd025d5cbb08a532fa6c431f95a9
fervi <lordfervi@protonmail.com>
2021-03-31 23:57:26 +0200

    Simply directory installer

---
+++ install/create_dirs.php
@@ -0,0 +1,17 @@
+<?php
+
+mkdir('../accounts');
+mkdir('../articles');
+mkdir('../avatars');
+mkdir('../images');
+mkdir('../indexes');
+mkdir('../indexes/antispam_articles');
+mkdir('../indexes/antispam_comments');
+mkdir('../indexes/comments');
+mkdir('../indexes/downvotes');
+mkdir('../indexes/mute');
+mkdir('../indexes/tags');
+mkdir('../indexes/tips');
+mkdir('../indexes/tips_self');
+mkdir('../indexes/upvotes');
+mkdir('../indexes/users');
